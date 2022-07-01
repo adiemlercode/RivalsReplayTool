3d8b024270d2f84fb0c7eef797c4f8ef07d2f10e
adiemlercode <108496205+adiemlercode@users.noreply.github.com>
2022-07-01 17:46:47 -0500

    Feature/add zip function (#3)
    * add comments
    
    * fix date string
    
    * add zip function

--- file_operations.py
+++ file_operations.py
@@ -5,21 +5,28 @@ import shutil
 
 
 def copy_most_recent_files(replay_folder, target_folder, number_of_files, set_name):
+    # verify folders exist
     if (not os.path.isdir(replay_folder)):
         raise FileNotFoundError("Could not find replays folder")
     if (not os.path.isdir(target_folder)):
         raise FileNotFoundError("Could not find destination folder")
-    
+
+    # get replays and sort by recency
     list_of_files = glob.glob(replay_folder + "/*")
     if (len(list_of_files) < number_of_files):
         raise FileNotFoundError("Could not find replays ")
     sorted_files = sorted(list_of_files, key=os.path.getmtime)
 
-    #make directory
+    # make set directory
     if (os.path.isdir(target_folder + "\\" + set_name)):
-        set_name = set_name + date.today()
+        set_name = set_name + str(date.today())
     os.mkdir(target_folder + "\\" + set_name)
     
+    #copy files over to set directory
     for i in range(number_of_files):
         file_name = sorted_files[i].split('\\')[-1]
         shutil.copyfile(sorted_files[i], target_folder + "\\" + set_name + "\\" + file_name)
+
+def zip_and_delete_set(set_folder):
+    shutil.make_archive(set_folder, "zip", set_folder)
+    shutil.rmtree(set_folder)
