94f53378652a4f522d533500739a58a5bad8bed2
Aaron Diemler <adiemler@gmail.com>
2022-07-01 17:27:01 -0500

    add comments

--- file_operations.py
+++ file_operations.py
@@ -5,21 +5,26 @@ import shutil
 
 
 def copy_most_recent_files(replay_folder, target_folder, number_of_files, set_name):
+    # verify folders exist
     if (not os.path.isdir(replay_folder)):
         raise FileNotFoundError("Could not find replays folder")
     if (not os.path.isdir(target_folder)):
         raise FileNotFoundError("Could not find destination folder")
-    
+
+    # get replays and sort by recency
     list_of_files = glob.glob(replay_folder + "/*")
     if (len(list_of_files) < number_of_files):
         raise FileNotFoundError("Could not find replays ")
     sorted_files = sorted(list_of_files, key=os.path.getmtime)
 
-    #make directory
+    # make set directory
     if (os.path.isdir(target_folder + "\\" + set_name)):
         set_name = set_name + date.today()
     os.mkdir(target_folder + "\\" + set_name)
     
+    #copy files over to set directory
     for i in range(number_of_files):
         file_name = sorted_files[i].split('\\')[-1]
         shutil.copyfile(sorted_files[i], target_folder + "\\" + set_name + "\\" + file_name)
+
+
